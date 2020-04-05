b23b8af9f2973c2b812d611451a19ffa162cf8a6
Filip Brankiewicz <filip.brankiewicz@gmail.com>
2020-04-06 00:56:14 +0100

    marker changed, added loading

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,12 +1,22 @@
 <template>
-  <gmap-map class="map" :center="center" :zoom="12" @click="(e) => getCabs(e)">
-    <gmap-marker
-      :key="index"
-      v-for="(m, index) in markers"
-      :position="m.position"
-      @click="center=m.position"
-    ></gmap-marker>
-  </gmap-map>
+  <section class="map__wrapper">
+    <transition name="fade">
+      <div class="map__loading" v-if="isLoading">
+        <img src="./../assets/images/loading.svg" alt="Loading..." />
+      </div>
+    </transition>
+    <h2 class="map__header">Click anywhere on the map to see all of the available cabs in this area!</h2>
+    <gmap-map class="map" :center="center" :zoom="12" @click="(e) => getCabs(e)">
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        @click="center=m.position"
+        :icon="{ url: require('./../assets/images/taxi.png'), scaledSize: {width: 50, height: 50},}"
+        class="map__marker"
+      ></gmap-marker>
+    </gmap-map>
+  </section>
 </template>
 
 <script>
@@ -65,8 +75,44 @@ export default {
 
 <style lang="scss">
 .map {
-  margin-top: 100px;
-  width: 1000px;
-  height: 600px;
+  width: 100%;
+  height: 100%;
+
+  &__header {
+    font-weight: 400;
+    margin-bottom: 20px;
+  }
+
+  &__wrapper {
+    margin-top: 60px;
+    position: relative;
+    width: 1000px;
+    height: 600px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  &__loading {
+    z-index: 10;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(white, 0.8);
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
